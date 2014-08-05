cbf1577aa3ce3222d83643fba216e5e2aa95e3d1
Administrator <sigamani@cern.ch>
2014-08-06 00:39:06 +0200

    newest update

--- signalRegionDefinitions.h
+++ signalRegionDefinitions.h
@@ -6,6 +6,7 @@ TString signalregionName(TString decaymode, int stopmass, int lspmass)
 {
 
     TString SR = "nan";
+
     if (decaymode == "T2bw075") 
     {
         if (lspmass > stopmass - 200)
@@ -22,13 +23,10 @@ TString signalregionName(TString decaymode, int stopmass, int lspmass)
 
     if (decaymode == "T2bw050") 
     {
-        if (lspmass > stopmass - 250) 
-        {
+        if (lspmass > stopmass - 250){
             SR = "T2bw050_1_lowLSP";
             if (lspmass > 50) SR = "T2bw050_1_highLSP"; 
- 
         }
-
         if (lspmass <= stopmass - 250 && lspmass > stopmass - 400)
             SR = "T2bw050_3";
         if (lspmass <= stopmass - 400 && lspmass > stopmass - 450)
@@ -51,7 +49,7 @@ TString signalregionName(TString decaymode, int stopmass, int lspmass)
             SR = "T2bw025_3_lowDM";
           if (lspmass <= stopmass - 275 && lspmass > stopmass - 425){ 
             SR = "T2bw025_3_lowDM";          
-                      if (lspmass > 50) SR = "T2bw025_3_highDM";
+          if (lspmass > 50) SR = "T2bw025_3_highDM";
 	   }
           if (lspmass <= stopmass - 425 && lspmass > stopmass - 600)
             SR = "T2bw025_4"; 
@@ -63,11 +61,9 @@ TString signalregionName(TString decaymode, int stopmass, int lspmass)
     if (decaymode == "T2tt") 
     {
 
-        //if ((lspmass > stopmass - 225 ) && (lspmass <= 100))  
-        if ((lspmass > stopmass - 225 ) && (lspmass < 100))  
+        if ((lspmass > stopmass - 225 ) && (lspmass <= 100))  
             SR = "T2tt_1_lowLSP"; 
-        //if ((lspmass > stopmass - 225 ) && (lspmass > 100))   
-        if ((lspmass > stopmass - 225 ) && (lspmass >= 100))   
+        if ((lspmass > stopmass - 225 ) && (lspmass > 100))   
 	    SR = "T2tt_1_highLSP"; 
         if (lspmass <= stopmass - 225 && lspmass > stopmass - 275)
             SR = "T2tt_2_lowDM";
@@ -88,80 +84,6 @@ TString signalregionName(TString decaymode, int stopmass, int lspmass)
 
 /*
 
-
-
-double signalcut(TString decaymode, int stopmass, int lspmass)
-{
-    double cutvalue = 0.;
-
-    if (decaymode == "T2bw075") 
-    {
-        if (lspmass > stopmass - 200)
-            cutvalue = 0.225;
-        if (lspmass <= stopmass - 200 && lspmass > stopmass - 325)
-            cutvalue = 0.275;
-        if (lspmass <= stopmass - 325 && lspmass > stopmass - 475)
-            cutvalue = 0.350;
-        if (lspmass <= stopmass - 475)
-            cutvalue = 0.25;
-        if (lspmass <= stopmass - 575)
-            cutvalue = 0.3;
-    }
-
-    if (decaymode == "T2bw050") 
-    {
-        if (lspmass > stopmass - 250) 
-        {
-            cutvalue = 0.25 ; 
-            if (lspmass < 150) cutvalue = 0.175;
-        }
-
-        if (lspmass <= stopmass - 250 && lspmass > stopmass - 400)
-            cutvalue = 0.3;
-        if (lspmass <= stopmass - 400 && lspmass > stopmass - 450)
-            cutvalue = 0.250;
-        if (lspmass <= stopmass - 450 && lspmass > stopmass - 625)
-            cutvalue = 0.225;
-        if (lspmass <= stopmass - 625)
-            cutvalue = 0.2;
-    }    
-
-    if (decaymode == "T2bw025") 
-    {
-
-          if (lspmass > stopmass - 225)
-            cutvalue = 0.15;
-          if (lspmass <= stopmass - 225 && lspmass > stopmass - 275)
-            cutvalue = 0.15;
-          if (lspmass <= stopmass - 275 && lspmass > stopmass - 425)
-            cutvalue = 0.3;
-          if (lspmass <= stopmass - 425 && lspmass > stopmass - 600)
-            cutvalue = 0.15;
-          if (lspmass <= stopmass - 600)
-            cutvalue = 0.175;
-
-   }
-    if (decaymode == "T2tt") 
-    { 
-
-        if (lspmass > stopmass - 225)
-            cutvalue = 0.275;
-        if (lspmass <= stopmass - 225 && lspmass > stopmass - 275)
-            cutvalue = 0.350;
-        if (lspmass <= stopmass - 275 && lspmass > stopmass - 325)
-            cutvalue = 0.4;
-        if (lspmass <= stopmass - 325 && lspmass > stopmass - 400)
-            cutvalue = 0.35;
-        if (lspmass <= stopmass - 400 && lspmass > stopmass - 500)
-            cutvalue = 0.325;
-        if (lspmass <= stopmass - 500)
-            cutvalue = 0.375;
-
-    }
-
-
-    return cutvalue;
-}
 
 int returnIntBDTOffsetValue(TString decaymode, int stopmass, int lspmass)
 {
@@ -251,7 +173,7 @@ int returnIntBDTOffsetValue(TString decaymode, int stopmass, int lspmass)
 
 double BDToutput(TString BDTregion)
 {
-    if (BDTregion == "T2tt_1_lowLSP" ) return myEvent.BDT_output_t2tt_R1;
+    	 if (BDTregion == "T2tt_1_lowLSP" ) return myEvent.BDT_output_t2tt_R1;
     else if (BDTregion == "T2tt_1_highLSP")	return myEvent.BDT_output_t2tt_R1;
     else if (BDTregion == "T2tt_2_lowDM" ) return myEvent.BDT_output_t2tt_R2;
     else if (BDTregion == "T2tt_2_highDM" ) return myEvent.BDT_output_t2tt_R2;
@@ -289,7 +211,7 @@ double BDToutput(TString BDTregion)
 
 double BDTcut(TString BDTregion)
 {
-    if (BDTregion == "T2tt_1_lowLSP" ) return 0.275;
+    	 if (BDTregion == "T2tt_1_lowLSP" ) 	return 0.275;
     else if (BDTregion == "T2tt_1_highLSP" )	return 0.425;
     else if (BDTregion == "T2tt_2_lowDM" )	return 0.35 ;
     else if (BDTregion == "T2tt_2_highDM" )	return 0.4 ;
@@ -297,25 +219,25 @@ double BDTcut(TString BDTregion)
     else if (BDTregion == "T2tt_5_mediumDM")	return 0.325;
     else if (BDTregion == "T2tt_5_highDM" )	return 0.375;
 
-    else if (BDTregion == "T2bw075_1" )	return 0.225;
-    else if (BDTregion == "T2bw075_2" )	return 0.275;
-    else if (BDTregion == "T2bw075_3" )	return 0.350;
+    else if (BDTregion == "T2bw075_1" )		return 0.225;
+    else if (BDTregion == "T2bw075_2" )		return 0.275;
+    else if (BDTregion == "T2bw075_3" )		return 0.350;
     else if (BDTregion == "T2bw075_5_lowDM" )	return 0.25 ;
     else if (BDTregion == "T2bw075_5_highDM")	return 0.3 ;
 
     else if (BDTregion == "T2bw050_1_lowLSP")	return 0.075;
     else if (BDTregion == "T2bw050_1_highLSP")	return 0.225;
-    else if (BDTregion == "T2bw050_3" )	return 0.3 ;
-    else if (BDTregion == "T2bw050_4" )	return 0.250;
-    else if (BDTregion == "T2bw050_5_lowDM" ) return 0.225;
-    else if (BDTregion == "T2bw050_5_highDM" ) return 0.275;
-    else if (BDTregion == "T2bw050_6" ) return 0.2 ; 
+    else if (BDTregion == "T2bw050_3" )		return 0.3 ;
+    else if (BDTregion == "T2bw050_4" )		return 0.250;
+    else if (BDTregion == "T2bw050_5_lowDM" ) 	return 0.225;
+    else if (BDTregion == "T2bw050_5_highDM" ) 	return 0.275;
+    else if (BDTregion == "T2bw050_6" ) 	return 0.2 ; 
 
-    else if (BDTregion == "T2bw025_1" ) return 0.15 ;
-    else if (BDTregion == "T2bw025_3_lowDM" ) return 0.15 ;
-    else if (BDTregion == "T2bw025_3_highDM") return 0.3 ;
-    else if (BDTregion == "T2bw025_4" ) return 0.15 ;
-    else if (BDTregion == "T2bw025_6" ) return 0.125; 
+    else if (BDTregion == "T2bw025_1" ) 	return 0.15 ;
+    else if (BDTregion == "T2bw025_3_lowDM" ) 	return 0.15 ;
+    else if (BDTregion == "T2bw025_3_highDM") 	return 0.3 ;
+    else if (BDTregion == "T2bw025_4" ) 	return 0.15 ;
+    else if (BDTregion == "T2bw025_6" ) 	return 0.125; 
 
     else
     {
