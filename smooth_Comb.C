65544082fd7f5ccc69f123deed56c3dc3bafffd4
Administrator <sigamani@cern.ch>
2015-03-04 22:35:51 +0100

    adding files

---
+++ smooth_Comb.C
@@ -0,0 +1,20 @@
+
+double ReturnSmoothedLimit( double x, double y, double limit, TString decay_mode, TString EXP ){
+
+
+        if (decay_mode=="T2bw025") {
+
+            if ( x == 125 &&  y == 0 ) limit = 1.1;
+            if ( x == 150 &&  y == 0 ) limit = 1.1;
+		}
+
+
+        if (decay_mode=="T2bw075") {
+
+            if ( x == 125 &&  y == 0 ) limit = 1.1;
+		}
+
+
+		return limit; 
+
+}
